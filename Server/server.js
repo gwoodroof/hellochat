2ad698bd9e5a94af99fae1de47f72e7fea367398
Garrett Woodroof <gwoodroof@gmail.com>
2014-06-29 00:58:16 -0700

    last commit connected to the db

--- Server/server.js
+++ Server/server.js
@@ -5,34 +5,47 @@ var MongoClient = require('mongodb').MongoClient;
 
 var port = 3000;
 
-var config = {
-//    "USER"    : "",
-//    "PASS"    : "",
-    "HOST"    : "127.0.0.1",
-    "PORT"    : "27017",
-    "DATABASE" : "db"
-};
-
-var dbPath  = "mongodb://" +
-//    config.USER + ":" +
-//    config.PASS + "@" +
-    config.HOST + ":"+ 
-    config.PORT + "/"+ 
-    config.DATABASE;
-
 MongoClient.connect('mongodb://127.0.0.1/db', function(err, db){
   if(err) throw err;
   console.log("connected to mongodb!");
 
   var memberCollection = db.collection('members');
-  memberCollection.insert({name:"GW"},function(err, docs){
-    if(err) throw err;
-    console.log("inserted GW");
-  });
+  
   var chatCollection = db.collection('chats');
   chatCollection.insert({handle:"GW", msg:"Hello Mongo!"}, function(err, docs){
     if(err) throw err;
     console.log("insterted GW: Hello Mongo!");
+  });
+  io.on('connection', function(client){
+    console.log('client connected!');
+    client.emit('alert', { message: 'You are connected.' });
+    
+    var handle = "";
+    
+    client.on('submit-name', function(username){
+      handle = username;
+      memberCollection.insert({name:handle},function(err, docs){
+        if(err) throw err;
+        console.log("inserted: " + handle);
+      });
+
+      var chat = {handle:"server",msg:handle + " just joined."};
+      console.log(chat);
+      io.emit('message', chat);
+      io.emit('add-member', handle);
+    });
+    
+    client.on('message', function(chat){
+      console.log(chat);
+      io.emit('message', chat);
+    });
+    
+    client.on('disconnect', function(){
+      var chat = {handle:handle,msg:"just left."};
+      console.log(chat);
+      client.broadcast.emit('message', chat);
+      client.broadcast.emit('remove-member', handle);
+    });
   });
 });
 
@@ -54,29 +67,4 @@ app.get('/app.js', function(request, response){
   response.sendfile("/opt/hellochat/Client/app.js");
 });
 
-io.on('connection', function(client){
-  console.log('client connected!');
-  client.emit('alert', { message: 'You are connected.' });
-  
-  var handle = "";
-  
-  client.on('submit-name', function(username){
-    handle = username;
-    var chat = {handle:"server",msg:handle + " just joined."};
-    console.log(chat);
-    io.emit('message', chat);
-    io.emit('add-member', handle);
-  });
-  
-  client.on('message', function(chat){
-    console.log(chat);
-    io.emit('message', chat);
-  });
-  
-  client.on('disconnect', function(){
-    var chat = {handle:handle,msg:"just left."};
-    console.log(chat);
-    client.broadcast.emit('message', chat);
-    client.broadcast.emit('remove-member', handle);
-  });
-});
+
