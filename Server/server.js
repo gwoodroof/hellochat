ce695e12db145b97200b79becd223783300df298
Garrett Woodroof <gwoodroof@gmail.com>
2014-06-29 08:51:35 -0700

    added callback to remove function

--- Server/server.js
+++ Server/server.js
@@ -51,9 +51,11 @@ MongoClient.connect('mongodb://127.0.0.1/db', function(err, db){
     client.on('disconnect', function(){
       var chat = {handle:handle,msg:" just left."};
       console.log(chat);
-      memberCollection.remove({name:handle});
-      client.broadcast.emit('message', chat);
-      client.broadcast.emit('remove-member', handle);
+      memberCollection.remove({name:handle}, function(err, numRemoved){
+        console.log("documents removed: " + numRemoved);
+        client.broadcast.emit('message', chat);
+        client.broadcast.emit('remove-member', handle);
+      });
     });
   });
 });
