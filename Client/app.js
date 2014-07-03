d695be59b5b0dc6bf6286e2c5516e32950e63bb1
Garrett Woodroof <gwoodroof@gmail.com>
2014-07-03 06:45:46 -0700

    scroll to the bottom... animated

--- Client/app.js
+++ Client/app.js
@@ -36,7 +36,7 @@ var chatController = app.controller('ChatController', function($scope){
     console.log('calling receiveChat method');
     $scope.chats.push(chat);
     $scope.$digest();
-    $('messages-box').animate({scrollTop: $('messages-box').scrollHeight}, "slow");
+    $('#messages-box').animate({scrollTop: $('#messages-box').scrollHeight}, "slow");
   };
 
   $scope.sendChat = function(chat){
