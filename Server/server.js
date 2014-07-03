173a27126542903db9ee6d90a71823edcd14d8ac
Garrett Woodroof <gwoodroof@gmail.com>
2014-07-03 07:01:03 -0700

    limit sever chatCollection to 3

--- Server/server.js
+++ Server/server.js
@@ -9,7 +9,7 @@ MongoClient.connect('mongodb://127.0.0.1/db', function(err, db){
   if(err) throw err;
   console.log("connected to mongodb!");
 
-  var memberCollection = db.collection('members');
+  var memberCollection = db.createCollection('members');
 /*
   memberCollection.drop(function(err,res){
     if(err) {
@@ -19,7 +19,7 @@ MongoClient.connect('mongodb://127.0.0.1/db', function(err, db){
   });
 */
   //create the collection and limit it to the lesser of ~20KB or 3 documents
-  var chatCollection = db.collection('chats',{ 'capped' : true, 'size' : 20000, 'max' : 3 });
+  var chatCollection = db.createCollection('chats',{ 'capped' : true, 'size' : 20000, 'max' : 3 });
 /*
   chatCollection.drop(function(err,res){
     if(err) {
